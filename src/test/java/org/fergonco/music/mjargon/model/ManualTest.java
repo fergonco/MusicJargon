2fe9c724d9f62f17e00aff8e96a0aebea7f040e0
Fernando González Cortés <fernando.gonzalez@geomati.co>
2017-09-08 06:32:51 +0200

    manual test

--- src/test/java/org/fergonco/music/mjargon/model/ManualTest.java
+++ src/test/java/org/fergonco/music/mjargon/model/ManualTest.java
@@ -4,7 +4,7 @@ import java.io.File;
 
 public class ManualTest {
 	public static void main(String[] args) throws Exception {
-		ModelTest.testWrite(new File("src/test/resources/pfrhythm.mjargon"));
+		ModelTest.testWrite(new File("src/test/resources/3_4.mjargon"));
 		ModelTest.playMidi();
 	}
 }
