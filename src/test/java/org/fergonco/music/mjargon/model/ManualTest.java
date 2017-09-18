1026196f29d3e540e3591fca9871b93c52552be8
Fernando González Cortés <fernando.gonzalez@geomati.co>
2017-09-18 15:29:51 +0200

    Don't use test code to run script

--- src/test/java/org/fergonco/music/mjargon/model/ManualTest.java
+++ src/test/java/org/fergonco/music/mjargon/model/ManualTest.java
@@ -1,10 +1,24 @@
 package org.fergonco.music.mjargon.model;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+
+import org.apache.commons.io.IOUtils;
+import org.fergonco.music.mjargon.lexer.Lexer;
+import org.fergonco.music.mjargon.lexer.Token;
+import org.fergonco.music.mjargon.parser.Parser;
 
 public class ManualTest {
 	public static void main(String[] args) throws Exception {
-		ModelTest.testWrite(new File("src/test/resources/script.mjargon"));
+		InputStream is = new FileInputStream(new File("src/test/resources/ternary.mjargon"));
+		String script = IOUtils.toString(is, "utf-8");
+		is.close();
+		Lexer lexer = new Lexer(script);
+		Token token = lexer.process();
+		Parser parser = new Parser();
+		Model model = parser.parse(token);
+		model.writeMidi(new File("/tmp/a.midi"));
 		ModelTest.playMidi();
 	}
 }
